6b04c222046e8e2934874e35442b64295f7738ca
pchisholm <chisholm.p@gmail.com>
2018-01-25 17:49:05 -0400

    minor clean up in analytics class

--- src/Analytics.ts
+++ src/Analytics.ts
@@ -23,35 +23,53 @@ export default class Analytics {
 		console.log('your guid: ' + this.guid)
 	}
 
+	/*
+		Sends events off to the GA collect API
+	 */
 	public send(event:any):void {
-		const url = this.formatString(event);
+		const url = this.concatParams(event);
 		this.makeRequest(url);
 	}
 
+	/*
+		Checks to see if a user has a cached GA client id
+		in their localStorage
+	 */
 	private checkCache():string {
 		try {
 			const cache = JSON.parse(localStorage.getItem(this.cachedKey));
 
+			// If cached creds obj exits
 			if (cache) {
+				// Check if expiry is still valid
 				if (cache.expiry > new Date()) {
 					return cache.guid;
 				} else {
+					// If the expiry is invalid, remove the creds
+					// and provide new ones
 					localStorage.removeItem(this.cachedKey);
 
 					return this.setCache(this.generateGuid());
 				}
 			} else {
+				// If a user has no cached creds, set new ones
 				return this.setCache(this.generateGuid());
 			}
 		} catch (e) {
+			// If any operations fail, return a guid for this session
 			return this.generateGuid();
 		}
 	}
 
+	/*
+		Sets new user creds in localStorage
+	 */
 	private setCache(guid:string):string {
 		try {
 			const expiry = new Date();
 
+			// Expiry is modeled after the GA client id cookie
+			// which expires after 2 years
 			const cache = {
 				guid: guid,
 				expiry: expiry.setFullYear(expiry.getFullYear() + 2)
@@ -65,19 +83,33 @@ export default class Analytics {
 		return guid;
 	}
 
+	/*
+		Generate random sequence 
+	 */
 	private s4():string {
 		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 	}
 
-	private getRandom():string {
-		return Math.round(new Date().getTime() / 1000).toString();
-	}
-
+	/*
+		Concatenate a new guid
+	 */
 	private generateGuid():string {
 		return `${this.s4()}${this.s4()}-${this.s4()}-${this.s4()}-${this.s4()}-${this.s4()}${this.s4()}${this.s4()}`;
 	}
 
-	private formatString(event:any):string {
+	/*
+		Get a random number to supply the caching buster parameter
+	 */
+	private getRandom():string {
+		return Math.round(new Date().getTime() / 1000).toString();
+	}
+
+	/*
+		Concatenates the default and event supplied parameters into a query string that
+		can be digested by the GA collect API. Any event provided params need to be
+		url encoded to prevent errors.
+	 */
+	private concatParams(event:any):string {
 		let queryString = `${this.baseUrl}?v=1&tid=${this.trackingId}&z=${this.getRandom()}&cid=${this.guid}`;
 
 		for (const param of Object.keys(event)) {
@@ -87,6 +119,9 @@ export default class Analytics {
 		return queryString;
 	}
 
+	/*
+		Makes GET request to GA collect API with formatted query string
+	 */
 	private makeRequest(url:string):void {
 		// make ga calls here
 		axios.get(url)
