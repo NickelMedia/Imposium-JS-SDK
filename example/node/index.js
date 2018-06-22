d30392c7bbcfb5b538b9b03585fdfe31251f15e6
pchisholm <chisholm.p@gmail.com>
2018-06-22 17:20:55 -0300

    clean up nodejs example

--- example/node/index.js
+++ example/node/index.js
@@ -12,50 +12,42 @@ const STORY_ID = '295a80d0-871c-4def-a579-8a375d6942fc',
         ACT_ID = '11a8e3b8-d3c4-4232-80a7-e757fb18fa57',
     ACCESS_KEY = 'zooch5ja8fiejoojoo6AeQuahmex1wes';
 
-const REPEAT_JOBS = 15;
-let jobsCompleted = 0;
-
 const client = new Imposium.ImposiumClient(ACCESS_KEY);
 
 const gotMessage = (data) => {
-	const {msg} = data;
-	console.log(`\n${YELLOW}Got message:${RESET} ${msg}`);
+	console.log(`${YELLOW}Got message:${RESET} ${data.msg}`);
 }
 
 const gotScene = (data) => {
-	console.log(`\n${CYAN}Got scene:${RESET}\n${JSON.stringify(data, null, 2)}`);
-
-	if (jobsCompleted <= REPEAT_JOBS) {
-		jobsCompleted++;
-		doRender();
-	} else {
-		console.log(`\n${CYAN}Test complete... exiting.${RESET}`)
-	}
+	const prettyData = JSON.stringify(data, null, 2);
+	console.log(`\n${CYAN}Got scene:${RESET}\n${prettyData}\n\n${YELLOW}Test complete! Exiting (0)...${RESET}`);
 }
 
 const onError = (error) => {
-	console.log(`\n${RED}Got error:${RESET}\n${error}\n\n${RED}Exiting (1)...${RESET}`);
+	console.error(`\n${RED}Got error:${RESET}\n${error}\n\n${RED}Exiting (1)...${RESET}`);
 	process.exit(1);
 }
 
 const doRender = () => {
 	const inventory = {
-		'text': 'Imposium && NodeJS',
-		'some_bad_prop': false,
-		'image': fs.createReadStream(__dirname + '/test.jpg'),
-		'callback_url': undefined
+		'text'         : 'test',
+		'image'        : fs.createReadStream(__dirname + '/test.jpg'),
+		'callback_url' : undefined
 	};
 
-	console.log(`\n${CYAN}Starting render #${jobsCompleted+1}${RESET}`);
 	client.renderVideo(STORY_ID, SCENE_ID, ACT_ID, inventory);
 }
 
-const main = () => {
+const bindEvents = () => {
 	client.on(Imposium.Events.GOT_MESSAGE, gotMessage);
 	client.on(Imposium.Events.GOT_SCENE, gotScene);
 	client.on(Imposium.Events.ERROR, onError);
+}
 
-	console.log(`${YELLOW}Starting Imposium test...${RESET}`);
+const main = () => {
+	console.log(`${YELLOW}Thanks for using Imposium!${RESET}\n`);
+
+	bindEvents();
 	doRender();
 }
 
