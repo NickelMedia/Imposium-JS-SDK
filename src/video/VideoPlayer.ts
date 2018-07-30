14966250e8df9de88ef94878c3f37ff1f8771111
Greg Webber <greg@jnickel.com>
2018-07-30 11:19:10 -0300

    working on linting rules

--- src/video/VideoPlayer.ts
+++ src/video/VideoPlayer.ts
@@ -5,154 +5,154 @@ import {isNode} from '../scaffolding/Helpers';
 import {EnvironmentError, PlayerConfigurationError} from '../scaffolding/Exceptions';
 
 export interface Video {
-	id       : string;
-	url      : string;
-	format   : string;
-	width    : number;
-	height   : number;
-	filesize : number;
-	duration : number;
-	rate     : number;
+    id       : string;
+    url      : string;
+    format   : string;
+    width    : number;
+    height   : number;
+    filesize : number;
+    duration : number;
+    rate     : number;
 }
 
 export interface VideoConfig {
-	poster : string;
-	videos : Video[];
+    poster : string;
+    videos : Video[];
 }
 
 const settings = require('../conf/settings.json').videoPlayer;
 
 export default abstract class VideoPlayer {
-	protected node:HTMLVideoElement = null;
+    protected node:HTMLVideoElement = null;
 
-	private static readonly intervalRate:number = settings.checkPlaybackRateMs;
-	private static readonly playbackEvents:number[] = settings.playbackEvents;
+    private static readonly intervalRate:number = settings.checkPlaybackRateMs;
+    private static readonly playbackEvents:number[] = settings.playbackEvents;
 
-	private readonly mediaEvents:any = {
-		loadstart : () => this.onLoad(),
-		play      : () => this.onPlay(),
-		pause     : () => this.onPause(),
-		ended     : () => this.onEnd()
-	};
+    private readonly mediaEvents:any = {
+        loadstart : () => this.onLoad(),
+        play      : () => this.onPlay(),
+        pause     : () => this.onPause(),
+        ended     : () => this.onEnd()
+    };
 
-	private experienceId:string = '';
-	private prevPlaybackEvent:number = 0;
-	private playbackInterval:any;
+    private experienceId:string = '';
+    private prevPlaybackEvent:number = 0;
+    private playbackInterval:any;
 
-	/*
-		Basis of Imposum/Fallback video player objects
-	 */
-	constructor(node:HTMLVideoElement) {
-		if (!isNode()) {
-			if (node instanceof HTMLVideoElement) {
-				const {mediaEvents} = this;
+    /*
+        Basis of Imposum/Fallback video player objects
+     */
+    constructor(node:HTMLVideoElement) {
+        if (!isNode()) {
+            if (node instanceof HTMLVideoElement) {
+                const {mediaEvents} = this;
 
-				this.node = node;
+                this.node = node;
 
-				for (const key of Object.keys(mediaEvents)) {
-					this.node.addEventListener(key, this.mediaEvents[key]);
-				}
-			} else {
-				// Prop passed wasn't of type HTMLVideoElement
-				throw new PlayerConfigurationError('invalidPlayerRef', null);
-			}
-		} else {
-			// Cancels out initialization in NodeJS
-			throw new EnvironmentError('node');
-		}
-	}
+                for (const key of Object.keys(mediaEvents)) {
+                    this.node.addEventListener(key, this.mediaEvents[key]);
+                }
+            } else {
+                // Prop passed wasn't of type HTMLVideoElement
+                throw new PlayerConfigurationError('invalidPlayerRef', null);
+            }
+        } else {
+            // Cancels out initialization in NodeJS
+            throw new EnvironmentError('node');
+        }
+    }
 
-	/*
-		Set the current experience id per job that gets passed to analytics calls
-	 */
-	protected setExperienceId = (experienceId:string) => {
-		this.experienceId = experienceId;
-	}
+    /*
+        Set the current experience id per job that gets passed to analytics calls
+     */
+    protected setExperienceId = (experienceId:string) => {
+        this.experienceId = experienceId;
+    }
 
-	/*
-		Placeholder, called when experiences get generated if a
-		player ref is set on a client instance
-	 */
-	public experienceGenerated = (experience:any):void => {}
+    /*
+        Placeholder, called when experiences get generated if a
+        player ref is set on a client instance
+     */
+    public experienceGenerated = (experience:any):void => {}
 
 
-	/*
-		Record a video "view" event when the player loads metadata successfully
-	 */
-	private onLoad = ():void => {
-		const {experienceId} = this;
+    /*
+        Record a video "view" event when the player loads metadata successfully
+     */
+    private onLoad = ():void => {
+        const {experienceId} = this;
 
-		Analytics.send({
-			t  : 'event',
-			ec : 'video_player',
-			ea : 'view',
-			el : experienceId
-		});
-	}
+        Analytics.send({
+            t  : 'event',
+            ec : 'video_player',
+            ea : 'view',
+            el : experienceId
+        });
+    }
 
-	/*
-		Start an interval that runs during playback which triggers playback
-		analytics calls
-	 */
-	private onPlay = ():void => {
-		clearInterval(this.playbackInterval);
+    /*
+        Start an interval that runs during playback which triggers playback
+        analytics calls
+     */
+    private onPlay = ():void => {
+        clearInterval(this.playbackInterval);
 
-		this.playbackInterval = setInterval(
-			() => this.checkPlayback(), 
-			VideoPlayer.intervalRate
-		);
-	}
+        this.playbackInterval = setInterval(
+            () => this.checkPlayback(), 
+            VideoPlayer.intervalRate
+        );
+    }
 
-	/*
-		Clear the interval on pause to ensure no false analytics calls occur
-	 */
-	private onPause = ():void => {
-		const {playbackInterval} = this;
-		clearInterval(playbackInterval);
-	}
+    /*
+        Clear the interval on pause to ensure no false analytics calls occur
+     */
+    private onPause = ():void => {
+        const {playbackInterval} = this;
+        clearInterval(playbackInterval);
+    }
 
-	/*
-		Clear the playback interval and emit a final playback analytics call
-	 */
-	private onEnd = ():void => {
-		const {experienceId, playbackInterval} = this;
+    /*
+        Clear the playback interval and emit a final playback analytics call
+     */
+    private onEnd = ():void => {
+        const {experienceId, playbackInterval} = this;
 
-		clearInterval(playbackInterval);
+        clearInterval(playbackInterval);
 
-		Analytics.send({
-			t  : 'event',
-			ec : 'video_player',
-			ea : 'playback_1',
-			el : experienceId
-		});
+        Analytics.send({
+            t  : 'event',
+            ec : 'video_player',
+            ea : 'playback_1',
+            el : experienceId
+        });
 
-		this.prevPlaybackEvent = 0;
-	}
+        this.prevPlaybackEvent = 0;
+    }
 
-	/*
-		Logic that checks to see what playback event should be fire based
-		on the current playback progress, clears the interval if the node
-		is / becomes un set to prevent bad calls
-	 */
-	private checkPlayback = ():void => {
-		const {
-			node,
-			prevPlaybackEvent,
-			experienceId, 
-			playbackInterval
-		} = this;
+    /*
+        Logic that checks to see what playback event should be fire based
+        on the current playback progress, clears the interval if the node
+        is / becomes un set to prevent bad calls
+     */
+    private checkPlayback = ():void => {
+        const {
+            node,
+            prevPlaybackEvent,
+            experienceId,
+            playbackInterval
+        } = this;
 
         if (node) {
-        	const {currentTime, duration} = node;
+            const {currentTime, duration} = node;
             const perc = currentTime / duration;
-           	const next = VideoPlayer.playbackEvents[prevPlaybackEvent];
+            const next = VideoPlayer.playbackEvents[prevPlaybackEvent];
 
             if (perc > next) {                
                 Analytics.send({
-                	t  : 'event',
-                	ec : 'video_player',
-                	ea : `playback_${next}`,
-					el : experienceId
+                    t  : 'event',
+                    ec : 'video_player',
+                    ea : `playback_${next}`,
+                    el : experienceId
                 });
 
                 this.prevPlaybackEvent++;
@@ -160,16 +160,16 @@ export default abstract class VideoPlayer {
         } else {
             clearInterval(playbackInterval);
         }
-	}
+    }
 
-	/*
-		Remove set events set on the supplied player reference
-	 */
-	public remove = ():void => {
-		const {mediaEvents, node} = this;
+    /*
+        Remove set events set on the supplied player reference
+     */
+    public remove = ():void => {
+        const {mediaEvents, node} = this;
 
-		for (const key of Object.keys(mediaEvents)) {
-			node.removeEventListener(key, mediaEvents[key]);
-		}
-	}
+        for (const key of Object.keys(mediaEvents)) {
+            node.removeEventListener(key, mediaEvents[key]);
+        }
+    }
 }
