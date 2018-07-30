f04f5495aba7c1b9729ea3e52b88d67cc595f38d
Greg Webber <greg@jnickel.com>
2018-07-30 12:22:44 -0300

    lint ImposiumClient

--- src/client/ImposiumClient.ts
+++ src/client/ImposiumClient.ts
@@ -1,4 +1,4 @@
-import "babel-polyfill";
+import 'babel-polyfill';
 
 import API from './http/API';
 import MessageConsumer from './tcp/MessageConsumer';
@@ -8,339 +8,337 @@ import FallbackPlayer from '../video/FallbackPlayer';
 import ExceptionPipe from '../scaffolding/ExceptionPipe';
 
 import {
-	ClientConfigurationError,
-	EnvironmentError,
-	NetworkError
+    ClientConfigurationError,
+    EnvironmentError,
+    NetworkError
 } from '../scaffolding/Exceptions';
 
 import {
-	prepConfig,
-	keyExists,
-	cloneWithKeys,
-	isFunc,
-	isNode
+    prepConfig,
+    keyExists,
+    cloneWithKeys,
+    isFunc,
+    isNode
 } from '../scaffolding/Helpers';
 
-interface ClientConfig {
-	accessToken : string;
-	storyId     : string;
-	actId       : string;
-	sceneId     : string;
-	environment : string;
+interface IClientConfig {
+    accessToken: string;
+    storyId: string;
+    actId: string;
+    sceneId: string;
+    environment: string;
 }
 
 const settings = require('../conf/settings.json').client;
 
 /*
-	Log out Imposium.js Version in the console
+    Log out Imposium.js Version in the console
  */
 const version = '[AIV]{version}[/AIV]';
-console.log(`%cPowered By%c Imposium%c v${version}%c https://imposium.com`, "text-transform:uppercase; padding:5px 0px 5px 5px; background-color:black; color:white;", "text-transform:uppercase; padding:5px 0px 5px 0px; background-color:black; color:#a1b83a;", "padding:5px 5px 5px 0px; background-color:black; color:white;", "padding:5px 5px 5px 0px;");
+console.log(`%cPowered By%c Imposium%c v${version}%c https: //imposium.com`,
+    'text-transform: uppercase; padding: 5px 0px 5px 5px; background-color: black; color: white;',
+    'text-transform: uppercase; padding: 5px 0px 5px 0px; background-color: black; color: #a1b83a;',
+    'padding: 5px 5px 5px 0px; background-color: black; color: white;',
+    'padding: 5px 5px 5px 0px;');
 
 export default class ImposiumClient {
-	public events = {
-		EXPERIENCE_CREATED : 'EXPERIENCE_CREATED',
-		UPLOAD_PROGRESS    : 'UPLOAD_PROGRESS',
-		GOT_EXPERIENCE     : 'GOT_EXPERIENCE',
-		STATUS_UPDATE      : 'STATUS_UPDATE',
-		ERROR              : 'ERROR'
-	};
+    public events = {
+        EXPERIENCE_CREATED : 'EXPERIENCE_CREATED',
+        UPLOAD_PROGRESS    : 'UPLOAD_PROGRESS',
+        GOT_EXPERIENCE     : 'GOT_EXPERIENCE',
+        STATUS_UPDATE      : 'STATUS_UPDATE',
+        ERROR              : 'ERROR'
+    };
 
-	private eventDelegateRefs:any = cloneWithKeys(this.events);
-	private api:API = null;
-	private player:VideoPlayer = null;
-	private consumer:MessageConsumer = null;
-	private clientConfig:ClientConfig = null;
-	private gaProperty:string = '';
+    private eventDelegateRefs: any = cloneWithKeys(this.events);
+    private api: API = null;
+    private player: VideoPlayer = null;
+    private consumer: MessageConsumer = null;
+    private clientConfig: IClientConfig = null;
+    private gaProperty: string = '';
 
-	/*
-		Initialize Imposium client
-	 */
-	constructor(config:any) {
-		if (config.accessToken && config.storyId) {
-			this.assignConfigOpts(config);
-		} else {
-			// do throw setup err, make new one for this that spits our creds??
-		}
-	}
+    /*
+        Initialize Imposium client
+     */
+    constructor(config: any) {
+        if (config.accessToken && config.storyId) {
+            this.assignConfigOpts(config);
+        } else {
+            // do throw setup err, make new one for this that spits our creds??
+        }
+    }
 
-	/*
-		Exposed for users who may want to re-use a client for n stories
-	 */
-	public setup = (config:any) => {
-		this.assignConfigOpts(config);
-	}
+    /*
+        Exposed for users who may want to re-use a client for n stories
+     */
+    public setup = (config: any) => {
+        this.assignConfigOpts(config);
+    }
 
-	/*
-		Set current video player ref
-	 */
-	public setPlayer = (player:VideoPlayer) => {
-		this.player = player;
-	}
+    /*
+        Set current video player ref
+     */
+    public setPlayer = (player: VideoPlayer) => {
+        this.player = player;
+    }
 
-	/*
-		Copies supplied config object to settings for sharing with sub components
-	 */
-	private assignConfigOpts = (config:any) => {
-		const prevConfig = this.clientConfig;
-		const {defaultConfig} = settings;
+    /*
+        Sets a callback for an event
+     */
+    public on = (eventName: string, callback: any): void => {
+        const {eventDelegateRefs, eventDelegateRefs: {ERROR}} = this;
 
-		prepConfig(config, defaultConfig);
-		this.clientConfig = {...defaultConfig, ...config};
+        try {
+            if (isFunc(callback)) {
+                if (keyExists(this.events, eventName)) {
+                    eventDelegateRefs[eventName] = callback;
+                } else {
+                    throw new ClientConfigurationError('invalidEventName', eventName);
+                }
+            } else {
+                throw new ClientConfigurationError('invalidCallbackType', eventName);
+            }
+        } catch (e) {
+            ExceptionPipe.trapError(e, ERROR);
+        }
+    }
 
-		if (!this.api) {
-			this.api = new API(this.clientConfig.accessToken, this.clientConfig.environment);
-		} else {
-			this.api.configureClient(this.clientConfig.accessToken, this.clientConfig.environment);
-		}
+    /*
+        Turns off a specific event or all events
+     */
+    public off = (eventName: string = ''): void => {
+        const {eventDelegateRefs, eventDelegateRefs: {ERROR}} = this;
 
-		// Prep for analytics in browser
-		if (!isNode()) {
-			this.getAnalyticsProperty();
-		}
-	}
+        try {
+            if (eventName !== '') {
+                if (keyExists(this.events, eventName)) {
+                    eventDelegateRefs[eventName] = null;
+                } else {
+                    throw new ClientConfigurationError('invalidEventName', eventName);
+                }
+            } else {
+                Object.keys(this.events).forEach((event) => {
+                    eventDelegateRefs[event] = null;
+                });
+            }
+        } catch (e) {
+            ExceptionPipe.trapError(e, ERROR);
+        }
+    }
 
-	/*
-		Get the GA property per storyId passed in
-	 */
-	private getAnalyticsProperty = ():void => {
-		const {api, player, clientConfig: {storyId}, eventDelegateRefs: {ERROR}} = this;
+    /*
+        Get experience data
+     */
+    public getExperience = (experienceId: string): void => {
+        const {api, player, gaProperty, eventDelegateRefs: {GOT_EXPERIENCE, ERROR}} = this;
 
-		api.getStory(storyId)
-		.then((story:any) => {
-			const {gaTrackingId} = story;
+        try {
+            if (GOT_EXPERIENCE || player) {
+                api.getExperience(experienceId)
+                .then((experience: any) => {
+                    const {output, id} = experience;
 
-			this.gaProperty = gaTrackingId;
+                    if (Object.keys(output).length > 0) {
+                        if (player) {
+                            player.experienceGenerated(experience);
+                        }
 
-			console.log(player)
-			if (player) {
-				player.setGaProperty(gaTrackingId);
-			}
+                        if (GOT_EXPERIENCE) {
+                            GOT_EXPERIENCE(experience);
+                        }
+                    } else {
+                        this.renderExperience(id);
+                    }
+                })
+                .catch((e) => {
+                    const wrappedError = new NetworkError('httpFailure', experienceId, e);
+                    ExceptionPipe.trapError(wrappedError, ERROR);
+                });
+            } else {
+                throw new ClientConfigurationError('eventNotConfigured', this.events.GOT_EXPERIENCE);
+            }
+        } catch (e) {
+            ExceptionPipe.trapError(e, ERROR);
+        }
+    }
 
-			Analytics.setup();
+    /*
+        Create new experience & return relevant meta
+     */
+    public createExperience = (inventory: any, render: boolean = true): void => {
+        const {
+            player,
+            eventDelegateRefs: {
+                GOT_EXPERIENCE,
+                EXPERIENCE_CREATED,
+                UPLOAD_PROGRESS,
+                ERROR
+            }
+        } = this;
 
-			this.doPageView();
-			window.addEventListener('popstate', () => this.doPageView());
-		})
-		.catch((e) => {
-			const wrappedError = new NetworkError('httpFailure', null, e);
-			ExceptionPipe.trapError(wrappedError, ERROR);
-		});
-	}
+        const permitRender = (render && (player || EXPERIENCE_CREATED));
+        const permitCreate = (!render && GOT_EXPERIENCE);
+        try {
+            if (permitRender || permitCreate) {
+                const {api, clientConfig: {storyId}} = this;
 
-	/*
-		Emit a GA page view event each time popstate occurs or the ga prop gets set
-	 */
-	private doPageView = ():void => {
-		const {gaProperty} = this;
+                api.postExperience(storyId, inventory, UPLOAD_PROGRESS)
+                .then((experience: any) => {
+                    const {clientConfig: {sceneId, actId}} = this;
+                    const {id} = experience;
 
-		Analytics.send({
-			prp : gaProperty,
-			t   : 'pageview', 
-			dp  : window.location.pathname
-		});
-	}
+                    if (EXPERIENCE_CREATED) {
+                        EXPERIENCE_CREATED(experience);
+                    }
+                    if (render) {
+                        this.renderExperience(id);
+                    }
+                })
+                .catch((e) => {
+                    const wrappedError = new NetworkError('httpFailure', null, e);
+                    ExceptionPipe.trapError(wrappedError, ERROR);
+                });
+            } else {
+                let eventType = null;
 
-	/*
-		Invokes the streaming process
-	 */
-	private startMessaging = (experienceId):void => {
-		const {api, eventDelegateRefs: {ERROR}} = this;
+                if (render && !GOT_EXPERIENCE) {
+                    eventType = this.events.GOT_EXPERIENCE;
+                }
+                if (!render && !EXPERIENCE_CREATED) {
+                    eventType = this.events.EXPERIENCE_CREATED;
+                }
 
-		api.invokeStream(experienceId)
-		.catch((e) => {
-			const wrappedError = new NetworkError('httpFailure', experienceId, e);
-			ExceptionPipe.trapError(wrappedError, ERROR);
-		});
-	}
+                throw new ClientConfigurationError('eventNotConfigured', eventType);
+            }
+        } catch (e) {
+            ExceptionPipe.trapError(e, ERROR);
+        }
+    }
 
-	/*
-		Make a new consumer w/ delegates
-	 */
-	private makeConsumer = (experienceId:string):void => {
-		const {clientConfig: {environment}, eventDelegateRefs, player} = this;
+    /*
+        Sets up analytics using fallback video player wrapper class
+     */
+    public captureAnalytics = (playerRef: HTMLVideoElement = null): void => {
+        const {eventDelegateRefs: {onError}} = this;
 
-		// Merge scoped startMessaging call with client events
-		const delegates:any = {
-			start: (id:string) => this.startMessaging(id),
-			...eventDelegateRefs
-		};
+        try {
+            if (!isNode()) {
+                this.setPlayer(new FallbackPlayer(playerRef));
+            } else {
+                throw new EnvironmentError('node');
+            }
+        } catch (e) {
+            ExceptionPipe.trapError(e, onError);
+        }
+    }
 
-		this.consumer = new MessageConsumer(
-			environment,
-			experienceId,
-			delegates,
-			player
-		);
-	}
+    /*
+        Copies supplied config object to settings for sharing with sub components
+     */
+    private assignConfigOpts = (config: any) => {
+        const prevConfig = this.clientConfig;
+        const {defaultConfig} = settings;
 
-	/*
-		Sets a callback for an event
-	 */
-	public on = (eventName:string, callback:any):void => {
-		const {eventDelegateRefs, eventDelegateRefs: {ERROR}} = this;
+        prepConfig(config, defaultConfig);
+        this.clientConfig = {...defaultConfig, ...config};
 
-		try {
-			if (isFunc(callback)) {
-				if (keyExists(this.events, eventName)) {
-					eventDelegateRefs[eventName] = callback;
-				} else {
-					throw new ClientConfigurationError('invalidEventName', eventName);
-				}
-			} else {
-				throw new ClientConfigurationError('invalidCallbackType', eventName);
-			}
-		} catch (e) {
-			ExceptionPipe.trapError(e, ERROR);
-		}
-	}
+        if (!this.api) {
+            this.api = new API(this.clientConfig.accessToken, this.clientConfig.environment);
+        } else {
+            this.api.configureClient(this.clientConfig.accessToken, this.clientConfig.environment);
+        }
 
-	/*
-		Turns off a specific event or all events
-	 */
-	public off = (eventName:string = ''):void => {
-		const {eventDelegateRefs, eventDelegateRefs: {ERROR}} = this;
+        // Prep for analytics in browser
+        if (!isNode()) {
+            this.getAnalyticsProperty();
+        }
+    }
 
-		try {
-			if (eventName !== '') {
-				if (keyExists(this.events, eventName)) {
-					eventDelegateRefs[eventName] = null;
-				} else {
-					throw new ClientConfigurationError('invalidEventName', eventName);
-				}
-			} else {
-				Object.keys(this.events).forEach((event) => {
-					eventDelegateRefs[event] = null;
-				});
-			}
-		} catch (e) {
-			ExceptionPipe.trapError(e, ERROR);
-		}
-	}
+    /*
+        Get the GA property per storyId passed in
+     */
+    private getAnalyticsProperty = (): void => {
+        const {api, player, clientConfig: {storyId}, eventDelegateRefs: {ERROR}} = this;
 
-	/*
-		Get experience data
-	 */
-	public getExperience = (experienceId:string):void => {
-		const {api, player, gaProperty, eventDelegateRefs: {GOT_EXPERIENCE, ERROR}} = this;
+        api.getStory(storyId)
+        .then((story: any) => {
+            const {gaTrackingId} = story;
 
-		try {
-			if (GOT_EXPERIENCE || player) {
-				api.getExperience(experienceId)
-				.then((experience:any) => {
-					const {output, id} = experience;
+            this.gaProperty = gaTrackingId;
+            console.log(player);
+            if (player) {
+                player.setGaProperty(gaTrackingId);
+            }
 
-					if (Object.keys(output).length > 0) {
-						if (player) {
-							player.experienceGenerated(experience);
-						}
+            Analytics.setup();
 
-						if (GOT_EXPERIENCE) {
-							GOT_EXPERIENCE(experience);
-						}
-					} else {
-						this.renderExperience(id);
-					}
-				})
-				.catch((e) => {
-					const wrappedError = new NetworkError('httpFailure', experienceId, e);
-					ExceptionPipe.trapError(wrappedError, ERROR);
-				});
-			} else {
-				throw new ClientConfigurationError('eventNotConfigured', this.events.GOT_EXPERIENCE);
-			}
-		} catch (e) {
-			ExceptionPipe.trapError(e, ERROR);
-		}
-	}
+            this.doPageView();
+            window.addEventListener('popstate', () => this.doPageView());
+        })
+        .catch((e) => {
+            const wrappedError = new NetworkError('httpFailure', null, e);
+            ExceptionPipe.trapError(wrappedError, ERROR);
+        });
+    }
 
-	/*
-		Create new experience & return relevant meta
-	 */
-	public createExperience = (inventory:any, render:boolean = true):void => {
-		const {
-			player, 
-			eventDelegateRefs: {
-				GOT_EXPERIENCE,
-				EXPERIENCE_CREATED,
-				UPLOAD_PROGRESS,
-				ERROR
-			}
-		} = this;
+    /*
+        Emit a GA page view event each time popstate occurs or the ga prop gets set
+     */
+    private doPageView = (): void => {
+        const {gaProperty} = this;
 
-		const permitRender = (render && (player || EXPERIENCE_CREATED));
-		const permitCreate = (!render && GOT_EXPERIENCE);
- 
-		try {
-			if (permitRender || permitCreate) {
-				const {api, clientConfig: {storyId}} = this;
+        Analytics.send({
+            prp: gaProperty,
+            t: 'pageview',
+            dp: window.location.pathname
+        });
+    }
 
-				api.postExperience(storyId, inventory, UPLOAD_PROGRESS)
-				.then((experience:any) => {
-					const {clientConfig: {sceneId, actId}} = this;
-					const {id} = experience;
+    /*
+        Invokes the streaming process
+     */
+    private startMessaging = (experienceId): void => {
+        const {api, eventDelegateRefs: {ERROR}} = this;
 
-					if (EXPERIENCE_CREATED) {
-						EXPERIENCE_CREATED(experience);
-					}
-					
-					if (render) {
-						this.renderExperience(id);
-					}
-				})
-				.catch((e) => {
-					const wrappedError = new NetworkError('httpFailure', null, e);
-					ExceptionPipe.trapError(wrappedError, ERROR);
-				});
-			} else {
-				let eventType = null;
+        api.invokeStream(experienceId)
+        .catch((e) => {
+            const wrappedError = new NetworkError('httpFailure', experienceId, e);
+            ExceptionPipe.trapError(wrappedError, ERROR);
+        });
+    }
 
-				if (render && !GOT_EXPERIENCE) {
-					eventType = this.events.GOT_EXPERIENCE;
-				} 
+    /*
+        Make a new consumer w/ delegates
+     */
+    private makeConsumer = (experienceId: string): void => {
+        const {clientConfig: {environment}, eventDelegateRefs, player} = this;
 
-				if (!render && !EXPERIENCE_CREATED) {
-					eventType = this.events.EXPERIENCE_CREATED;
-				}
+        // Merge scoped startMessaging call with client events
+        const delegates: any = {
+            start: (id: string) => this.startMessaging(id),
+            ...eventDelegateRefs
+        };
 
-				throw new ClientConfigurationError('eventNotConfigured', eventType);
-			}
-		} catch (e) {
-			ExceptionPipe.trapError(e, ERROR);
-		}
-	}
+        this.consumer = new MessageConsumer(
+            environment,
+            experienceId,
+            delegates,
+            player
+        );
+    }
 
-	/*
-		Invokes rendering processes and starts listening for messages 
-	 */
-	private renderExperience = (experienceId:string):void => {
-		const {consumer} = this;
-		
-		if (!consumer) {
-			this.makeConsumer(experienceId);
-		} else {
-			consumer.kill()
-			.then(() => {
-				this.makeConsumer(experienceId);
-			});
-		}
-	}
-
-	/*
-		Sets up analytics using fallback video player wrapper class
-	 */
-	public captureAnalytics = (playerRef:HTMLVideoElement = null):void => {
-		const {eventDelegateRefs: {onError}} = this;
-
-		try {
-			if (!isNode()) {
-				this.setPlayer(new FallbackPlayer(playerRef));
-			} else {
-				throw new EnvironmentError('node');
-			}
-		} catch (e) {
-			ExceptionPipe.trapError(e, onError);
-		}
-	}
-
+    /*
+        Invokes rendering processes and starts listening for messages
+     */
+    private renderExperience = (experienceId: string): void => {
+        const {consumer} = this;
+        if (!consumer) {
+            this.makeConsumer(experienceId);
+        } else {
+            consumer.kill()
+            .then(() => {
+                this.makeConsumer(experienceId);
+            });
+        }
+    }
 }
