cc8b34bdc13f20c5ed343dc73d0f23debc53999c
Greg Webber <greg@jnickel.com>
2018-07-30 12:15:47 -0300

    ExceptionPipe linting

--- src/scaffolding/ExceptionPipe.ts
+++ src/scaffolding/ExceptionPipe.ts
@@ -3,11 +3,11 @@ import {ImposiumError} from './Exceptions';
 const warnings = require('../conf/warnings.json');
 
 export default class ExceptionPipe {
-    public static logWarning = (type:string, messageKey:string):void => {
+    public static logWarning = (type: string, messageKey: string): void => {
         console.warn(`IMPOSIUM\n${warnings[type][messageKey]}`);
     }
 
-    public static trapError = (e:ImposiumError, errorEvent:(e:any)=>any = null):void => {
+    public static trapError = (e: ImposiumError, errorEvent: (e: any) => any = null): void => {
         if (errorEvent) {
             errorEvent(e);
         }
