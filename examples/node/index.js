b71fba144d22b9e8b0847066695c254a0d8ff24c
Greg Webber <greg@jnickel.com>
2018-08-03 14:36:57 -0300

    clean up Node sample

--- examples/node/index.js
+++ examples/node/index.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const Imposium = require('../../lib/imposium.js');
 
 const YELLOW = '\x1b[33m',
-        CYAN = '\x1b[36m',
-         RED = '\x1b[31m',
-       RESET = '\x1b[0m';
+	    CYAN = '\x1b[36m',
+	     RED = '\x1b[31m',
+	   RESET = '\x1b[0m';
 
 const config = {
    accessToken : 'zooch5ja8fiejoojoo6AeQuahmex1wes',
@@ -14,47 +14,47 @@ const config = {
 const imposium = new Imposium.Client(config);
 
 const statusUpdate = (data) => {
-    console.log(`${YELLOW}Got message:${RESET} ${data.status || data.msg}`);
+	console.log(`${YELLOW}Got message:${RESET} ${data.status || data.msg}`);
 }
 
 const experienceCreated = () => {
-    console.log(`\n${CYAN}Experience Created!${RESET}\n`);
+	console.log(`\n${CYAN}Experience Created!${RESET}\n`);
 }
 
 const gotExperience = (data) => {
-    const prettyData = JSON.stringify(data, null, 2);
-    console.log(`\n${CYAN}Got scene:${RESET}\n${prettyData}\n\n${YELLOW}Test complete! Exiting...${RESET}`);
+	const prettyData = JSON.stringify(data, null, 2);
+	console.log(`\n${CYAN}Got scene:${RESET}\n${prettyData}\n\n${YELLOW}Test complete! Exiting...${RESET}`);
 }
 
 const onError = (error) => {
-    console.error(`\n${RED}Something went wrong:${RESET}\n`, error);
-    process.exit(1);
+	console.error(`\n${RED}Something went wrong:${RESET}\n`, error);
+	process.exit(1);
 }
 
 const doRender = () => {
-    const inventory = {
-        'text'         : 'test',
-        'image'        : fs.createReadStream(__dirname + '/test.jpg'),
-        'callback_url' : ''
-    };
+	const inventory = {
+		'text'         : 'test',
+		'image'        : fs.createReadStream(__dirname + '/test.jpg'),
+		'callback_url' : ''
+	};
 
-    imposium.createExperience(inventory);
+	imposium.createExperience(inventory);
 }
 
 const bindEvents = () => {
-    const {STATUS_UPDATE, EXPERIENCE_CREATED, GOT_EXPERIENCE, ERROR} = Imposium.Events;
+	const {STATUS_UPDATE, EXPERIENCE_CREATED, GOT_EXPERIENCE, ERROR} = Imposium.Events;
 
-    imposium.on(STATUS_UPDATE, statusUpdate);
-    imposium.on(EXPERIENCE_CREATED, experienceCreated);
-    imposium.on(GOT_EXPERIENCE, gotExperience);
-    imposium.on(ERROR, onError);
+	imposium.on(STATUS_UPDATE, statusUpdate);
+	imposium.on(EXPERIENCE_CREATED, experienceCreated);
+	imposium.on(GOT_EXPERIENCE, gotExperience);
+	imposium.on(ERROR, onError);
 }
 
 const main = () => {
-    console.log(`${YELLOW}Thanks for using Imposium!${RESET}`);
+	console.log(`${YELLOW}Thanks for using Imposium!${RESET}`);
 
-    bindEvents();
-    doRender();
+	bindEvents();
+	doRender();
 }
 
 main();
