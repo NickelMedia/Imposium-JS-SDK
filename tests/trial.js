66c7c48a8cf4568cd4c040d04270a3090193b5c5
pchisholm <chisholm.p@gmail.com>
2018-08-17 15:56:06 -0300

    parallelize testing flow

--- tests/trial.js
+++ tests/trial.js
@@ -1,5 +1,6 @@
 const fs        = require('fs');
 const assert    = require('assert');
+const parallel  = require('mocha.parallel');
 const webdriver = require('selenium-webdriver');
 const remote    = require('selenium-webdriver/remote');
 
@@ -8,81 +9,91 @@ const example  = '/examples/web/basic-deeplink.html';
 const caption  = 'This is a test.';
 const imgPath  = `${process.cwd()}/test.jpg`;
 const expect   = 'Video ready for viewing.'
-const waitFor  = 40000;
+const waitFor  = 50000;
 
-const {findElement, wait} = bsDriver;
 const {By: {id}} = webdriver;
 
-/*
-    Runs via selenium wait call (setInterval), strips the Imposium status message from DOM to validate if 
-    
- */
-const stripStatus = (cb) => {
-    return findElement(id('status'))
-    .then((e) => {
-        e.getText()
-        .then((t) => {
-            try {
-                assert.equal(t, expect);
-                done();
-            } catch(e) {
+const baseCapability = {
+    'browserstack.user': 'patrickchisholm1',
+    'browserstack.key': 'kqExpNPZDere7GszwkgL',
+    'browserstack.localIdentifier': 'sdktest',
+    'browserstack.local': 'true',
+    'resolution': '1024x768',
+    'os': 'OS X',
+    'os_version': 'High Sierra'
+};
 
-            }
-        });
-    });
+const requiredBrowsers = [
+    {
+        'browserName' : 'Safari',
+        'browser_version' : '11.0'
+    },
+    // {
+    //     'browserName' : 'Chrome',
+    //     'browser_version' : '68.0'
+    // },
+    // {
+    //     'browserName' : 'Firefox',
+    //     'browser_version' : '61.0'
+    // }
+];
+
+const capabilities = requiredBrowsers.map((browserSpec) => {
+    return {...baseCapability, ...browserSpec};
+});
+
+/*
+`   Returns a new browserstack driver instance (per browser spec)
+ */
+const getBsDriver = (capabilities) => {
+    let bsDriver = new webdriver.Builder()
+        .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+        .withCapabilities(capabilities)
+        .build();
+
+    // This will allow the driver to use local image files for testing
+    bsDriver.setFileDetector(new remote.FileDetector);
+
+    return bsDriver;
 }
 
-describe('deeplink', () => {
-    let driver;
+parallel('Deeplink Test Suite', () => {
+    capabilities.forEach((c) => {
+        it('Experience should be generated', (done) => {
+            const bsDriver = getBsDriver(c);
+            const {findElement, wait} = bsDriver;
 
-    before(() => {
-        /*
-            TO DO: Put this into a conf file and add a helper function to make this easily configurable
-            for parallel testing 
-         */
-        const capabilities = {
-            'browserName': 'Chrome',
-            'browser_version': '68.0',
-            'os': 'OS X',
-            'os_version': 'High Sierra',
-            'resolution': '1024x768',
-            'browserstack.user': 'patrickchisholm1',
-            'browserstack.key': 'kqExpNPZDere7GszwkgL',
-            'browserstack.local': 'true',
-            'browserstack.localIdentifier': 'sdktest'
-        };
+            bsDriver.get(`${tempHost}${example}`)
+            .then(() => {
+                Promise.all([
+                    bsDriver.findElement(webdriver.By.id('caption')).sendKeys(caption),
+                    bsDriver.findElement(webdriver.By.id('image')).sendKeys(imgPath)
+                ]).then(() => {
+                    bsDriver.findElement(webdriver.By.id('btn-submit')).click()
+                    .then(() => {
+                        wait(
+                            () => {
+                                return bsDriver.findElement(webdriver.By.id('status'))
+                                .then((e) => {
+                                    e.getText()
+                                    .then((t) => {
+                                        try {
+                                            assert.equal(t, expect);
 
-        // Bind selenium driver with BS config
-        bsDriver = new webdriver.Builder()
-            .usingServer('http://hub-cloud.browserstack.com/wd/hub')
-            .withCapabilities(capabilities)
-            .build();
+                                            bsDriver.quit(() => {
+                                                done();
+                                            });
+                                        } catch(e) {
 
-        // This will allow the driver to use local image files for testing
-        bsDriver.setFileDetector(new remote.FileDetector);
-    });
-
-    // TO DO: Make this more descriptive once functionality is locked
-    it('experience should load', (done) => {
-        bsDriver.get(`${tempHost}${example}`)
-        .then(() => {
-            Promise.all([
-                findElement(id('caption')).sendKeys(caption),
-                findElement(id('image')).sendKeys(imgPath)
-            ]).then(() => {
-                findElement(id('btn-submit')).click()
-                .then(() => {
-                    wait(stripStatus, waitFor);
+                                        }
+                                    });
+                                });
+                            },
+                            waitFor
+                        );
+                    });
                 });
             });
         });
     });
-
-    after((done) => {
-        bsDriver.quit()
-        .then(() => {
-            done();
-        });
-    });
 });
-
