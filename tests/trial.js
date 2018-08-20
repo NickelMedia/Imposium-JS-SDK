88c663b3e2e273393bacbbfb29505ee9f3c49ba9
pchisholm <chisholm.p@gmail.com>
2018-08-20 15:47:43 -0300

    spec out the new / existing experience test cases

--- tests/trial.js
+++ tests/trial.js
@@ -1,14 +1,16 @@
 const fs        = require('fs');
 const assert    = require('assert');
+const parallel  = require('mocha.parallel');
 const webdriver = require('selenium-webdriver');
 const remote    = require('selenium-webdriver/remote');
 
-const tempHost = 'http://patrickchisholm1.browserstack.com';
-const example  = '/examples/web/basic-deeplink.html';
-const caption  = 'This is a test.';
-const imgPath  = `${process.cwd()}/tests/test.jpg`;
-const expect   = 'Video ready for viewing.'
-const waitFor  = 50000;
+const tempHost     = 'http://patrickchisholm1.browserstack.com';
+const example      = '/examples/web/basic-deeplink.html';
+const experienceId = '#e8bb7871-3c60-4af5-9666-ca7d328da4c8';
+const caption      = 'This is a test.';
+const imgPath      = `${process.cwd()}/tests/test.jpg`;
+const expect       = 'Video ready for viewing.'
+const waitFor      = 50000;
 
 const {By: {id}} = webdriver;
 
@@ -41,57 +43,92 @@ const capabilities = requiredBrowsers.map((browserSpec) => {
     return {...baseCapability, ...browserSpec};
 });
 
-describe(`Deeplink: ${capabilities[0].browserName}`, () => {
-    let bsDriver;
+const doQuit = (driver, cb) => {
+    driver.quit()
+    .then(() => {
+        cb();
+    }); 
+}
 
-    before(() => {
-        bsDriver = new webdriver.Builder()
-            .usingServer('http://hub-cloud.browserstack.com/wd/hub')
-            .withCapabilities(capabilities[0])
-            .build();
+parallel('Deeplink Suite', () => {
+    capabilities.forEach((capability, i) => {
+        const {browserName} = capability;
 
-        // This will allow the driver to use local image files for testing
-        bsDriver.setFileDetector(new remote.FileDetector);
-    });
+        it(`${browserName} - Existing experience should load.`, (done) => {
+            const bsDriver = new webdriver.Builder()
+                .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+                .withCapabilities(capability)
+                .build();
 
-    it(`Experience should be generated`, (done) => {
-        bsDriver.get(`${tempHost}${example}`)
-        .then(() => {
-            bsDriver.findElement(webdriver.By.id('caption')).sendKeys(caption)
+            // This will allow the driver to use local image files for testing
+            bsDriver.setFileDetector(new remote.FileDetector);
+            
+            bsDriver.get(`${tempHost}${example}${experienceId}`)
             .then(() => {
-                bsDriver.findElement(webdriver.By.id('image')).sendKeys(imgPath)
-                .then(() => {
-                    bsDriver.findElement(webdriver.By.id('btn-submit')).click()
-                    .then(() => {
-                        bsDriver.wait(() => {
-                            return bsDriver.findElement(webdriver.By.id('status'))
-                            .then((statusElement) => {
-                                statusElement.getText()
-                                .then((strippedText) => {
-                                    try {
-                                        assert.equal(strippedText, 'Video ready for viewing.');
-                                    } catch(e) {
-
-                                    }
-                                });
-                            });
-                        }, waitFor)
-                        .then((v) => {
-                            done();
-                        })
-                        .catch((e) => {
-                            done();
-                        }); 
+                bsDriver.wait(() => {
+                    return bsDriver.findElement(webdriver.By.id('dynamic-video'))
+                    .then((player) => {
+                        return player.getAttribute('src')
+                        .then((src) => {
+                            return src.length === 145;
+                        });
                     });
+                }, 5000)
+                .then((v) => {
+                    doQuit(bsDriver, done);
+                })
+                .catch((e) => {
+                    doQuit(bsDriver, done);
                 });
             });
         });
-    });
 
-    after((done) => {
-        bsDriver.quit()
-        .then(() => {
-            done();
+        it(`${browserName} - New experience should be generated.`, (done) => {
+            const bsDriver = new webdriver.Builder()
+                .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+                .withCapabilities(capability)
+                .build();
+
+            // This will allow the driver to use local image files for testing
+            bsDriver.setFileDetector(new remote.FileDetector);
+
+            bsDriver.get(`${tempHost}${example}`)
+            .then(() => {
+                bsDriver.findElement(webdriver.By.id('caption')).sendKeys(caption)
+                .then(() => {
+                    bsDriver.findElement(webdriver.By.id('image')).sendKeys(imgPath)
+                    .then(() => {
+                        bsDriver.findElement(webdriver.By.id('btn-submit')).click()
+                        .then(() => {
+                            bsDriver.wait(() => {
+                                return bsDriver.findElement(webdriver.By.id('status'))
+                                .then((statusElement) => {
+                                    statusElement.getText()
+                                    .then((strippedText) => {
+                                        try {
+                                            assert.equal(strippedText, 'Video ready for viewing.');
+                                        } catch(e) {
+
+                                        }
+                                    });
+                                });
+                            }, waitFor)
+                            .then((v) => {
+                                bsDriver.quit()
+                                .then(() => {
+                                    done();
+                                });
+                            })
+                            .catch((e) => {
+                                bsDriver.quit()
+                                .then(() => {
+                                    done();
+                                });
+                            }); 
+                        });
+                    });
+                });
+            });
         });
     });
 });
