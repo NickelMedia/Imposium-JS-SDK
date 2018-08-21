89b7aadfd00e5bcd085ca9c98ff67c2e6e781692
pchisholm <chisholm.p@gmail.com>
2018-08-21 13:07:47 -0300

    clean up local example

--- tests/experience-local.js
+++ tests/experience-local.js
@@ -3,20 +3,27 @@ const parallel  = require('mocha.parallel');
 const webdriver = require('selenium-webdriver');
 const remote    = require('selenium-webdriver/remote');
 
-const base    = `file://${process.cwd()}/examples/web/basic-deeplink.html`;
-const hash    = '#e8bb7871-3c60-4af5-9666-ca7d328da4c8';
-const caption = 'This is a test.';
-const imgPath = `${process.cwd()}/tests/test.jpg`;
-const expect  = 'Video ready for viewing.'
+// URL data
+const base = `file://${process.cwd()}/examples/web/basic-deeplink.html`;
+const hash = '#e8bb7871-3c60-4af5-9666-ca7d328da4c8';
 
+// Existing experience test props
+const srcLengthExpected = 46;
 const existingTimeout = 5000;
-const freshTimeout = 50000;
 
+// New experience test props
+const caption         = 'This is a test.';
+const imgPath         = `${process.cwd()}/tests/test.jpg`;
+const captionExpected = 'Video ready for viewing.';
+const freshTimeout    = 50000;
+
+// Local drivers to test with
 const drivers = [
     'chrome',
     'firefox'
 ];
 
+// Exit with or without an error 
 const doQuit = (driver, cb, e = null) => {
     driver.quit()
     .then(() => {
@@ -24,8 +31,11 @@ const doQuit = (driver, cb, e = null) => {
     });
 }
 
+// Do local tests in parallel
 parallel('Experience Handling', () => {
     drivers.forEach((driverType) => {
+
+        // Get existing experience via deeplink example
         it(`Driver: ${driverType} - Existing experience should load.`, (done) => {
             const driver = new webdriver.Builder()
                 .forBrowser(driverType)
@@ -39,7 +49,7 @@ parallel('Experience Handling', () => {
                         .then((videoElement) => {
                             videoElement.getAttribute('src')
                             .then((src) => {
-                                resolve(src.length === 46);
+                                resolve(src.length === srcLengthExpected);
                             });
                         });
                     });
@@ -53,12 +63,13 @@ parallel('Experience Handling', () => {
             });
         });
 
+        // Generate a new experience via deeplink example
         it(`Driver: ${driverType} - New experience should be generated.`, (done) => {
             const driver = new webdriver.Builder()
                 .forBrowser(driverType)
                 .build();
 
-            // This will allow the driver to use local image files for testing
+            // This will allow the driver to use local image files for uploading
             driver.setFileDetector(new remote.FileDetector);
 
             driver.get(base)
@@ -75,7 +86,7 @@ parallel('Experience Handling', () => {
                                     .then((statusElement) => {
                                         statusElement.getText()
                                         .then((strippedText) => {
-                                            resolve(strippedText === expect);
+                                            resolve(strippedText === captionExpected);
                                         });
                                     });
                                 });
