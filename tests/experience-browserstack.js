e906f70c7d1b4bb210a0bb16b171efea185f3329
pchisholm <chisholm.p@gmail.com>
2018-08-21 13:24:21 -0300

    clean up remote example

--- tests/experience-browserstack.js
+++ tests/experience-browserstack.js
@@ -1,19 +1,24 @@
 const fs        = require('fs');
 const assert    = require('assert');
 const parallel  = require('mocha.parallel');
+const platforms = require('./platforms.json');
 const webdriver = require('selenium-webdriver');
 const remote    = require('selenium-webdriver/remote');
 
-const tempHost     = 'http://patrickchisholm1.browserstack.com';
-const example      = '/examples/web/basic-deeplink.html';
-const experienceId = '#e8bb7871-3c60-4af5-9666-ca7d328da4c8';
-const caption      = 'This is a test.';
-const imgPath      = `${process.cwd()}/tests/test.jpg`;
-const expect       = 'Video ready for viewing.'
-const waitFor      = 50000;
+const base = 'http://patrickchisholm1.browserstack.com/examples/web/basic-deeplink.html';
+const hash = '#e8bb7871-3c60-4af5-9666-ca7d328da4c8';
 
-const {By: {id}} = webdriver;
+// Existing experience test props
+const srcLengthExpected = 145;
+const existingTimeout = 5000;
 
+// New experience test props
+const caption         = 'This is a test.';
+const imgPath         = `${process.cwd()}/tests/test.jpg`;
+const captionExpected = 'Video ready for viewing.';
+const freshTimeout    = 50000;
+
+// Base capability that all Browserstack test specs need to share
 const baseCapability = {
     'browserstack.user': 'patrickchisholm1',
     'browserstack.key': 'kqExpNPZDere7GszwkgL',
@@ -21,97 +26,12 @@ const baseCapability = {
     'browserstack.local': 'true'
 };
 
-const platforms = [
-    {
-        'os': 'OS X',
-        'os_version': 'High Sierra',
-        'browserName' : 'Safari',
-        'browser_version' : '11.0',
-        'resolution': '1024x768'
-    },
-    {
-        'os': 'OS X',
-        'os_version': 'High Sierra',
-        'browserName' : 'Chrome',
-        'browser_version' : '68.0',
-        'resolution': '1024x768'
-    },
-    {
-        'os': 'OS X',
-        'os_version': 'High Sierra',
-        'browserName' : 'Firefox',
-        'browser_version' : '61.0',
-        'resolution': '1024x768'
-    },
-    {
-        'os': 'Windows',
-        'os_version': '10',
-        'browserName' : 'IE',
-        'browser_version' : '11.0',
-        'resolution': '1024x768'
-    },
-    {
-        'os': 'Windows',
-        'os_version': '8',
-        'browserName' : 'IE',
-        'browser_version' : '10.0',
-        'resolution': '1024x768'
-    },
-    {
-        'os': 'Windows',
-        'os_version': '10',
-        'browserName' : 'Edge',
-        'browser_version' : '17.0',
-        'resolution': '1024x768'
-    },
-    {
-        'device' : 'iPhone 8',
-        'os_version' : '11.0',
-        'realMobile' : 'true',
-        'browserName' : 'iPhone'
-    },
-    {
-        'device' : 'iPhone 7',
-        'os_version' : '10.3',
-        'realMobile' : 'true',
-        'browserName' : 'iPhone'
-    },
-    {
-        'device' : 'Google Pixel 2',
-        'os_version' : '8.0',
-        'realMobile' : 'true',
-        'browserName' : 'android'
-    },
-    {
-        'device' : 'Samsung Galaxy S8',
-        'os_version' : '7.0',
-        'realMobile' : 'true',
-        'browserName' : 'android'
-    },
-    {
-        'device' : 'Samsung Galaxy S7',
-        'os_version' : '6.0',
-        'realMobile' : 'true',
-        'browserName' : 'android'
-    },
-    {
-        'device' : 'Samsung Galaxy S6',
-        'os_version' : '5.0',
-        'realMobile' : 'true',
-        'browserName' : 'android'
-    },
-    {
-        'device' : 'Google Nexus 5',
-        'os_version' : '4.4',
-        'realMobile' : 'true',
-        'browserName' : 'android'
-    },
-];
-
+// Merge up all the platform objs with a copy of the 
 const capabilities = platforms.map((platform) => {
     return {...baseCapability, ...platform};
 });
 
+// Exit with or without an error 
 const doQuit = (driver, cb, e = null) => {
     driver.quit()
     .then(() => {
@@ -119,57 +39,58 @@ const doQuit = (driver, cb, e = null) => {
     });
 }
 
+// Do remote tests in parallel
 parallel('Deeplink Suite', () => {
     capabilities.forEach((capability, i) => {
         const {browserName} = capability;
 
         it(`${browserName} - Existing experience should load.`, (done) => {
-            const bsDriver = new webdriver.Builder()
+            const driver = new webdriver.Builder()
                 .usingServer('http://hub-cloud.browserstack.com/wd/hub')
                 .withCapabilities(capability)
                 .build();
 
             // This will allow the driver to use local image files for testing
-            bsDriver.setFileDetector(new remote.FileDetector);
+            driver.setFileDetector(new remote.FileDetector);
             
-            bsDriver.get(`${tempHost}${example}${experienceId}`)
+            driver.get(`${base}${hash}`)
             .then(() => {
-                bsDriver.wait(() => {
+                driver.wait(() => {
                     return new Promise((resolve, reject) => {
-                        bsDriver.findElement(webdriver.By.id('dynamic-video'))
+                        driver.findElement(webdriver.By.id('dynamic-video'))
                         .then((player) => {
                             player.getAttribute('src')
                             .then((src) => {
-                                resolve(src.length === 145);
+                                resolve(src.length === srcLengthExpected);
                             });
                         });
                     });
-                }, 5000)
+                }, existingTimeout)
                 .then((v) => {
-                    doQuit(bsDriver, done);
+                    doQuit(driver, done);
                 })
                 .catch((e) => {
-                    doQuit(bsDriver, done, e);
+                    doQuit(driver, done, e);
                 });
             });
         });
 
         it(`${browserName} - New experience should be generated.`, (done) => {
-            const bsDriver = new webdriver.Builder()
+            const driver = new webdriver.Builder()
                 .usingServer('http://hub-cloud.browserstack.com/wd/hub')
                 .withCapabilities(capability)
                 .build();
 
             // This will allow the driver to use local image files for testing
-            bsDriver.setFileDetector(new remote.FileDetector);
+            driver.setFileDetector(new remote.FileDetector);
 
-            bsDriver.get(`${tempHost}${example}`)
+            driver.get(base)
             .then(() => {
-                bsDriver.findElement(webdriver.By.id('caption')).sendKeys(caption)
+                driver.findElement(webdriver.By.id('caption')).sendKeys(caption)
                 .then(() => {
-                    bsDriver.findElement(webdriver.By.id('image')).sendKeys(imgPath)
+                    driver.findElement(webdriver.By.id('image')).sendKeys(imgPath)
                     .then(() => {
-                        bsDriver.findElement(webdriver.By.id('btn-submit')).click()
+                        driver.findElement(webdriver.By.id('btn-submit')).click()
                         .then(() => {
                             driver.wait(() => {
                                 return new Promise((resolve, reject) => {
@@ -177,7 +98,7 @@ parallel('Deeplink Suite', () => {
                                     .then((statusElement) => {
                                         statusElement.getText()
                                         .then((strippedText) => {
-                                            resolve(strippedText === expect);
+                                            resolve(strippedText === captionExpected);
                                         });
                                     });
                                 });
