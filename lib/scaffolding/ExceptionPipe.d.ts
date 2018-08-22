726ba30a06505ceab9460ac6523b045e1305e5a4
pchisholm <chisholm.p@gmail.com>
2018-08-22 14:08:43 -0300

    undo typescript types for time being

--- lib/scaffolding/ExceptionPipe.d.ts
+++
@@ -1,7 +0,0 @@
-export default class ExceptionPipe {
-    static logWarning: (type: string, messageKey: string) => void;
-    static trapError: (e: any, storyId: string, errorEvent?: (e: any) => any) => void;
-    private static readonly errorsProperty;
-    private static logError;
-    private static traceError;
-}
--- lib/video/FallbackPlayer.d.ts
+++
@@ -1,5 +0,0 @@
-import VideoPlayer from './VideoPlayer';
-export default class FallbackPlayer extends VideoPlayer {
-    constructor(node: HTMLVideoElement);
-    experienceGenerated: (experience: any) => void;
-}
