4ca36fa122fae9d41458c8e7e6fbea19e852ca11
pchisholm <chisholm.p@gmail.com>
2019-01-16 21:32:16 -0400

    simplify publish a little more

--- publish.sh
+++ publish.sh
@@ -2,44 +2,45 @@
 
 set -e
 
-git_branch=$(git branch | grep \* | cut -d ' ' -f2)
-
 print_checkout_step () {
     echo -e "\n==============================\n$1"
 }
 
-if [ "$git_branch" == "master" ]; then
-    current_version=$(cat ./package.json | jq -r ".version")
+current_git_branch=$(git branch | grep \* | cut -d ' ' -f2)
 
-    echo -e "Preparing for publication on NPM...\n"
+if [ $current_git_branch != "master" ]; then
+    echo "Please make sure to publish from the master branch. Currently on branch: $current_git_branch"
+    exit 1 
+fi
 
-    read -e -p "Please enter a new version number: "  fresh_version
-    read -e -p "Version $fresh_version will replace version $current_version, are you sure this is correct? [y / n]: " confirmation
+current_npm_version=$(cat ./package.json | jq -r ".version")
 
-    if [ "$confirmation" == "y" ]; then
-        echo "Process will exit on errors."
+echo -e "Preparing for publication on NPM...\n"
 
-        npm whoami > /dev/null 2>&1
-        if [ $? == 1 ]; then
-            echo "Please login to NPM..."
-            npm login
-        fi
+read -e -p "Please enter a new version number: "  fresh_version
+read -e -p "Version $fresh_version will replace version $current_version, are you sure this is correct? [y / n]: " confirmation
 
-        tmpFile=$(mktemp)
-        jq ".version = \"$fresh_version\"" ./package.json > "$tmpFile" && mv "$tmpFile" ./package.json
+if [ "$confirmation" == "y" ]; then
+    echo "Process will exit on errors."
 
-        print_checkout_step "Linting the project..."
-        tslint ./src/**/*.ts
-
-        print_checkout_step "Preparing bundled code..."
-        webpack --bail && webpack --bail -p || { exit 1; }
-
-        print_checkout_step "Generating type definitions...\n"
-        ./generate-types.sh
-
-        print_checkout_step "Publishing Imposium JS SDK"
-        npm publish --access=public
+    npm whoami > /dev/null 2>&1
+    if [ $? == 1 ]; then
+        echo "Please login to NPM..."
+        npm login
     fi
-else
-    echo "Please make sure to publish from the master branch. Currently on branch: $git_branch"
+
+    tmpFile=$(mktemp)
+    jq ".version = \"$fresh_version\"" ./package.json > "$tmpFile" && mv "$tmpFile" ./package.json
+
+    print_checkout_step "Linting the project..."
+    tslint ./src/**/*.ts
+
+    print_checkout_step "Preparing bundled code..."
+    webpack --bail && webpack --bail -p || { exit 1; }
+
+    print_checkout_step "Generating type definitions...\n"
+    ./generate-types.sh
+
+    print_checkout_step "Publishing Imposium JS SDK"
+    npm publish --access=public
 fi
